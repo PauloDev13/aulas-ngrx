52b2f1cf3c84741ea20217253ce6fcb020db4e12
PgmAdmin <prmorais1302@gmail.com>
2023-09-01 15:11:22 -0300

    Add Angular Material UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreModule } from '@ngrx/store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CounterButtonComponent } from './component/counter-button/counter-button.component';
 import { CounterDisplayComponent } from './component/counter-display/counter-display.component';
+import { AppMaterialModule } from './shared/app-material/app-material.module';
 import { counterReducer } from './shared/store/counter.reducer';
 
 @NgModule({
@@ -14,6 +16,8 @@ import { counterReducer } from './shared/store/counter.reducer';
     BrowserModule,
     AppRoutingModule,
     StoreModule.forRoot({ counter: counterReducer }),
+    BrowserAnimationsModule,
+    AppMaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
